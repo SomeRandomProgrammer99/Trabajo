b1f71da07b1551113688851c7af1a18d26d39328
Rex <programasauriorex@gmail.com>
2021-11-04 22:02:12 -0500

    update

--- src/main/java/pe/edu/upc/trabajo/controller/FrontController.java
+++ src/main/java/pe/edu/upc/trabajo/controller/FrontController.java
@@ -1,6 +1,6 @@
 package pe.edu.upc.trabajo.controller;
 
-/*
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,52 +10,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import pe.edu.upc.trabajo.business.crud.PostService;
-import pe.edu.upc.trabajo.models.entities.Post;
-import pe.edu.upc.trabajo.utils.ProductSearch;
+import pe.edu.upc.trabajo.business.crud.ProductService;
+import pe.edu.upc.trabajo.models.entities.Product;
+//import pe.edu.upc.trabajo.utils.ProductSearch;
 
 
 @Controller
 @RequestMapping("/")// es la carpeta raiz, si se crea otra carpeta
-de de escribe "/nombreDeLaCarpeta "/
+//de de escribe "/nombreDeLaCarpeta "/
 @SessionAttributes("{productSearch}")
 
 public class FrontController {
 
 	@Autowired 
-	private PostService //postService;
+	private ProductService productService; //postService;
 	
 	@GetMapping
 	public String index(Model model) {
-	ProductSearch productSearch = new ProductSearch();
+	//ProductSearch productSearch = new ProductSearch();
 		try {
-			List<Post> posts = postService.getAll();
-			model.addAttribute("posts",posts);
+			List<Product> posts = productService.getAll();
+			model.addAttribute("product",posts);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		model.addAttribute("productSearch", productSearch);
+		//model.addAttribute("productSearch", productSearch);
 		return "index";
 	}
 	
-	
-	
-	
-	@GetMapping// request , es el nombre que se pone en el navegador
-	public String index(Model model) {
-		
-		try {
-			List<Department> departments=departmentService.getAll();
-			model.addAttribute("departments",departments);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return "index";//es el nombre del html
-	}
-	
-	
-}*/
+}
